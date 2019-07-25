a1fdf0c6bf39699c74246c49686d567f7d87a624
letrend <st@gi.ai>
2019-07-26 01:07:19 +0200

    first draft for 30 motors

---
+++ snickerdoodle.srcs/sources_1/bd/roboy_plexus/hdl/roboy_plexus_wrapper.v
@@ -0,0 +1,224 @@
+//Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2019.1 (lin64) Build 2552052 Fri May 24 14:47:09 MDT 2019
+//Date        : Fri Jul 26 00:41:57 2019
+//Host        : base running 64-bit Ubuntu 18.04.2 LTS
+//Command     : generate_target roboy_plexus_wrapper.bd
+//Design      : roboy_plexus_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module roboy_plexus_wrapper
+   (DDR_addr,
+    DDR_ba,
+    DDR_cas_n,
+    DDR_ck_n,
+    DDR_ck_p,
+    DDR_cke,
+    DDR_cs_n,
+    DDR_dm,
+    DDR_dq,
+    DDR_dqs_n,
+    DDR_dqs_p,
+    DDR_odt,
+    DDR_ras_n,
+    DDR_reset_n,
+    DDR_we_n,
+    FIXED_IO_ddr_vrn,
+    FIXED_IO_ddr_vrp,
+    FIXED_IO_mio,
+    FIXED_IO_ps_clk,
+    FIXED_IO_ps_porb,
+    FIXED_IO_ps_srstb,
+    mirrored_muscle_unit_0,
+    mirrored_muscle_unit_1,
+    mirrored_muscle_unit_2,
+    mirrored_muscle_unit_3,
+    mirrored_muscle_unit_4,
+    mirrored_muscle_unit_5,
+    miso_0,
+    miso_1,
+    miso_2,
+    miso_3,
+    miso_4,
+    miso_5,
+    mosi_0,
+    mosi_1,
+    mosi_2,
+    mosi_3,
+    mosi_4,
+    mosi_5,
+    power_sense_n,
+    sck_0,
+    sck_1,
+    sck_2,
+    sck_3,
+    sck_4,
+    sck_5,
+    ss_n_0,
+    ss_n_1,
+    ss_n_2,
+    ss_n_3,
+    ss_n_4,
+    ss_n_5);
+  inout [14:0]DDR_addr;
+  inout [2:0]DDR_ba;
+  inout DDR_cas_n;
+  inout DDR_ck_n;
+  inout DDR_ck_p;
+  inout DDR_cke;
+  inout DDR_cs_n;
+  inout [3:0]DDR_dm;
+  inout [31:0]DDR_dq;
+  inout [3:0]DDR_dqs_n;
+  inout [3:0]DDR_dqs_p;
+  inout DDR_odt;
+  inout DDR_ras_n;
+  inout DDR_reset_n;
+  inout DDR_we_n;
+  inout FIXED_IO_ddr_vrn;
+  inout FIXED_IO_ddr_vrp;
+  inout [53:0]FIXED_IO_mio;
+  inout FIXED_IO_ps_clk;
+  inout FIXED_IO_ps_porb;
+  inout FIXED_IO_ps_srstb;
+  input mirrored_muscle_unit_0;
+  input mirrored_muscle_unit_1;
+  input mirrored_muscle_unit_2;
+  input mirrored_muscle_unit_3;
+  input mirrored_muscle_unit_4;
+  input mirrored_muscle_unit_5;
+  input miso_0;
+  input miso_1;
+  input miso_2;
+  input miso_3;
+  input miso_4;
+  input miso_5;
+  output mosi_0;
+  output mosi_1;
+  output mosi_2;
+  output mosi_3;
+  output mosi_4;
+  output mosi_5;
+  input power_sense_n;
+  output sck_0;
+  output sck_1;
+  output sck_2;
+  output sck_3;
+  output sck_4;
+  output sck_5;
+  output [7:0]ss_n_0;
+  output [7:0]ss_n_1;
+  output [5:0]ss_n_2;
+  output [3:0]ss_n_3;
+  output [5:0]ss_n_4;
+  output [3:0]ss_n_5;
+
+  wire [14:0]DDR_addr;
+  wire [2:0]DDR_ba;
+  wire DDR_cas_n;
+  wire DDR_ck_n;
+  wire DDR_ck_p;
+  wire DDR_cke;
+  wire DDR_cs_n;
+  wire [3:0]DDR_dm;
+  wire [31:0]DDR_dq;
+  wire [3:0]DDR_dqs_n;
+  wire [3:0]DDR_dqs_p;
+  wire DDR_odt;
+  wire DDR_ras_n;
+  wire DDR_reset_n;
+  wire DDR_we_n;
+  wire FIXED_IO_ddr_vrn;
+  wire FIXED_IO_ddr_vrp;
+  wire [53:0]FIXED_IO_mio;
+  wire FIXED_IO_ps_clk;
+  wire FIXED_IO_ps_porb;
+  wire FIXED_IO_ps_srstb;
+  wire mirrored_muscle_unit_0;
+  wire mirrored_muscle_unit_1;
+  wire mirrored_muscle_unit_2;
+  wire mirrored_muscle_unit_3;
+  wire mirrored_muscle_unit_4;
+  wire mirrored_muscle_unit_5;
+  wire miso_0;
+  wire miso_1;
+  wire miso_2;
+  wire miso_3;
+  wire miso_4;
+  wire miso_5;
+  wire mosi_0;
+  wire mosi_1;
+  wire mosi_2;
+  wire mosi_3;
+  wire mosi_4;
+  wire mosi_5;
+  wire power_sense_n;
+  wire sck_0;
+  wire sck_1;
+  wire sck_2;
+  wire sck_3;
+  wire sck_4;
+  wire sck_5;
+  wire [7:0]ss_n_0;
+  wire [7:0]ss_n_1;
+  wire [5:0]ss_n_2;
+  wire [3:0]ss_n_3;
+  wire [5:0]ss_n_4;
+  wire [3:0]ss_n_5;
+
+  roboy_plexus roboy_plexus_i
+       (.DDR_addr(DDR_addr),
+        .DDR_ba(DDR_ba),
+        .DDR_cas_n(DDR_cas_n),
+        .DDR_ck_n(DDR_ck_n),
+        .DDR_ck_p(DDR_ck_p),
+        .DDR_cke(DDR_cke),
+        .DDR_cs_n(DDR_cs_n),
+        .DDR_dm(DDR_dm),
+        .DDR_dq(DDR_dq),
+        .DDR_dqs_n(DDR_dqs_n),
+        .DDR_dqs_p(DDR_dqs_p),
+        .DDR_odt(DDR_odt),
+        .DDR_ras_n(DDR_ras_n),
+        .DDR_reset_n(DDR_reset_n),
+        .DDR_we_n(DDR_we_n),
+        .FIXED_IO_ddr_vrn(FIXED_IO_ddr_vrn),
+        .FIXED_IO_ddr_vrp(FIXED_IO_ddr_vrp),
+        .FIXED_IO_mio(FIXED_IO_mio),
+        .FIXED_IO_ps_clk(FIXED_IO_ps_clk),
+        .FIXED_IO_ps_porb(FIXED_IO_ps_porb),
+        .FIXED_IO_ps_srstb(FIXED_IO_ps_srstb),
+        .mirrored_muscle_unit_0(mirrored_muscle_unit_0),
+        .mirrored_muscle_unit_1(mirrored_muscle_unit_1),
+        .mirrored_muscle_unit_2(mirrored_muscle_unit_2),
+        .mirrored_muscle_unit_3(mirrored_muscle_unit_3),
+        .mirrored_muscle_unit_4(mirrored_muscle_unit_4),
+        .mirrored_muscle_unit_5(mirrored_muscle_unit_5),
+        .miso_0(miso_0),
+        .miso_1(miso_1),
+        .miso_2(miso_2),
+        .miso_3(miso_3),
+        .miso_4(miso_4),
+        .miso_5(miso_5),
+        .mosi_0(mosi_0),
+        .mosi_1(mosi_1),
+        .mosi_2(mosi_2),
+        .mosi_3(mosi_3),
+        .mosi_4(mosi_4),
+        .mosi_5(mosi_5),
+        .power_sense_n(power_sense_n),
+        .sck_0(sck_0),
+        .sck_1(sck_1),
+        .sck_2(sck_2),
+        .sck_3(sck_3),
+        .sck_4(sck_4),
+        .sck_5(sck_5),
+        .ss_n_0(ss_n_0),
+        .ss_n_1(ss_n_1),
+        .ss_n_2(ss_n_2),
+        .ss_n_3(ss_n_3),
+        .ss_n_4(ss_n_4),
+        .ss_n_5(ss_n_5));
+endmodule
---
+++ snickerdoodle.srcs/sources_1/bd/roboy_plexus/ip/roboy_plexus_myoControl_0_4/roboy_plexus_myoControl_0_4_stub.v
@@ -0,0 +1,52 @@
+// Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2019.1 (lin64) Build 2552052 Fri May 24 14:47:09 MDT 2019
+// Date        : Fri Jul 26 00:42:57 2019
+// Host        : base running 64-bit Ubuntu 18.04.2 LTS
+// Command     : write_verilog -force -mode synth_stub -rename_top roboy_plexus_myoControl_0_4 -prefix
+//               roboy_plexus_myoControl_0_4_ roboy_plexus_myoControl_0_3_stub.v
+// Design      : roboy_plexus_myoControl_0_3
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg400-3
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* X_CORE_INFO = "myoControl_v1_0,Vivado 2019.1" *)
+module roboy_plexus_myoControl_0_4(myocontrol_awaddr, myocontrol_awprot, 
+  myocontrol_awvalid, myocontrol_awready, myocontrol_wdata, myocontrol_wstrb, 
+  myocontrol_wvalid, myocontrol_wready, myocontrol_bresp, myocontrol_bvalid, 
+  myocontrol_bready, myocontrol_araddr, myocontrol_arprot, myocontrol_arvalid, 
+  myocontrol_arready, myocontrol_rdata, myocontrol_rresp, myocontrol_rvalid, 
+  myocontrol_rready, ss_n_o, miso, mosi, sck, mirrored_muscle_unit, power_sense_n, 
+  myocontrol_aclk, myocontrol_aresetn)
+/* synthesis syn_black_box black_box_pad_pin="myocontrol_awaddr[10:0],myocontrol_awprot[2:0],myocontrol_awvalid,myocontrol_awready,myocontrol_wdata[31:0],myocontrol_wstrb[3:0],myocontrol_wvalid,myocontrol_wready,myocontrol_bresp[1:0],myocontrol_bvalid,myocontrol_bready,myocontrol_araddr[10:0],myocontrol_arprot[2:0],myocontrol_arvalid,myocontrol_arready,myocontrol_rdata[31:0],myocontrol_rresp[1:0],myocontrol_rvalid,myocontrol_rready,ss_n_o[7:0],miso,mosi,sck,mirrored_muscle_unit,power_sense_n,myocontrol_aclk,myocontrol_aresetn" */;
+  input [10:0]myocontrol_awaddr;
+  input [2:0]myocontrol_awprot;
+  input myocontrol_awvalid;
+  output myocontrol_awready;
+  input [31:0]myocontrol_wdata;
+  input [3:0]myocontrol_wstrb;
+  input myocontrol_wvalid;
+  output myocontrol_wready;
+  output [1:0]myocontrol_bresp;
+  output myocontrol_bvalid;
+  input myocontrol_bready;
+  input [10:0]myocontrol_araddr;
+  input [2:0]myocontrol_arprot;
+  input myocontrol_arvalid;
+  output myocontrol_arready;
+  output [31:0]myocontrol_rdata;
+  output [1:0]myocontrol_rresp;
+  output myocontrol_rvalid;
+  input myocontrol_rready;
+  output [7:0]ss_n_o;
+  input miso;
+  output mosi;
+  output sck;
+  input mirrored_muscle_unit;
+  input power_sense_n;
+  input myocontrol_aclk;
+  input myocontrol_aresetn;
+endmodule
